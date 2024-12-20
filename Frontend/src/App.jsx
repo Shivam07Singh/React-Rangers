d18ad184699e6a3254bdd06b7023de7813af62a5
varad-pawar1 <varadpawarm@gmail.com>
2024-12-20 21:28:50 +0530

    varad-day3

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,28 +1,27 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Dashboard from "./Components/Dashboard";
-import Template1 from "./Components/Template1";
-import Template2 from "./Components/Template2";
-import ConstructionTemplate from "./Components/Template3";
-import LibraryTemplate from "./Components/Template5";
-import HotelBookingTemplate from "./Components/Template6";
-import "./styles.css";
+
+import Template2 from "./bComponent/Template2";
+import ConstructionTemplate from "./bComponent/Template3";
+
 import Login from "./sComponent/Login";
 import SignUp from "./sComponent/signup";
+import Dashboard from "./bComponent/Dashboard";
+import Template1 from "./bComponent/Template1";
 
 export default function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/" element={<Dashboard />} />
           <Route path="/Template1" element={<Template1 />} />
           <Route path="/Template2" element={<Template2 />} />
           <Route path="/Template3" element={<ConstructionTemplate />} />
           {/* <Route path="/Template5" element={<LibraryTemplate />} />
           <Route path="/Template6" element={<HotelBookingTemplate />} /> */}
-          <Route path="./sComponent/Login" element={<Login/>} />
-          <Route path="./sComponent/signup" element={<SignUp/>} />
+
         </Routes>
       </div>
     </Router>
