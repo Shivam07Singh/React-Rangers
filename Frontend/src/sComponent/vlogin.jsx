1a7f413df3b50dc1dc4fffbeacd73b6c3425ba79
varad-pawar1 <varadpawarm@gmail.com>
2024-12-29 23:31:29 +0530

    Ready to delopy code error solve10

--- Frontend/src/sComponent/vlogin.jsx
+++ Frontend/src/sComponent/vlogin.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { Link, useNavigate, } from "react-router-dom";
-import "./Login.css";
+import "./vlogin.css";
 
 const Login = () => {
   let navigate = useNavigate()
