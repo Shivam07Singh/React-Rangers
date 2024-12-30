2f84abc8b9504326d270d12aef9375f4d0c32f48
varad-pawar1 <varadpawarm@gmail.com>
2024-12-30 10:46:03 +0530

    Ready to delopy code error solve11

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ import { websiteRoutes } from "./routes/websiteRoutes.js";
 const app = express();
 const PORT = process.env.PORT
 
-app.use(cors({ origin: process.env.CORS_ORIGIN.split(",") })); 
+app.use(cors({ origin: process.env.CORS_ORIGIN.split(",") }));
 
 app.use(morgan("common"));
 app.use(compression());
