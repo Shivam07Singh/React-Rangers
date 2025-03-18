8ebd4282989da62b72ff89fb3672465b02b5b338
Shivam07Singh <shivamsingh735r@gmail.com>
2025-03-18 21:35:33 +0530

    Final Project Done

--- Frontend/src/components/NicheTemplates.jsx
+++ Frontend/src/components/NicheTemplates.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import professionalTemplates from "../assets/Professional-templates.png";
+
 
 const NicheTemplates = () => {
   return (
     <section className="niche-templates">
       <div className="niche-image">
-        <img
-          src="https://www.yola.com/ws/media-library/d90805f1237a4a64afaa8ac1ac1a5159/website-templates-1.webp"
-          alt="Templates Preview"
-        />
+        <img src={professionalTemplates} alt="Templates Preview" />
       </div>
       <div className="niche-content">
-        <h2>Professional-looking niche templates</h2>
+        <h2>Professional-looking templates</h2>
         <p>
-          We have plenty of beautiful, conversion-optimized, and mobile-friendly templates tailored to specific businesses.
+          We have plenty of beautiful, conversion-optimized, and mobile-friendly templates tailored
+          to specific businesses.
         </p>
         <div className="niche-buttons">
-
-          <button className="btn-start" ><Link to="/dashboard" style={{ color: 'white' }}>Start Building </Link></button>
+          <button className="btn-start">
+            <Link to="/dashboard" style={{ color: "white" }}>
+              Start Building{" "}
+            </Link>
+          </button>
           <button className="btn-explore">Explore Templates</button>
         </div>
       </div>
